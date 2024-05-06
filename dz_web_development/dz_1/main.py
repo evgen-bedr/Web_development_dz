60a8b423665cd4d52ca292709f36502d6d66c238
redrif48 <redrif48@gmail.com>
2024-05-06 23:20:41 +0200

    add dz_1

---
+++ dz_web_development/dz_1/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+
+@app.route('/user/<user_name>')
+def return_name(user_name):
+    return f"Hallo {user_name}"
+
+
+if __name__ == '__main__':
+    app.run()
